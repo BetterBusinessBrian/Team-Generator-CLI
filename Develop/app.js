ad533c7def80c4eef710af9c59a02a025aed443a
BetterBusinessBrian <bman1402@gmail.com>
2020-11-10 10:42:04 -0500

    implement cleaner if statements and reduce codebase size

--- Develop/app.js
+++ Develop/app.js
@@ -4,46 +4,41 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
-
 const render = require("./lib/htmlRenderer");
 var employees = [];
-
 
 // Start
 console.log("Please build your team.")
 
 // Function to add Manager (run first)
- async function addManager(){
-let answers = await inquirer.prompt([
-    {
-        type: "input",
-        name: "name",
-        message: "What is your Manager's name?"
-    },
-    {
-        type: 'input',
-        name: "id",
-        message: "What is your Manager's id?"
-    },
-    {
-        type: 'input',
-        name: "email",
-        message: "What is your Manager's email?"
-    },
-    {
-        type: 'input',
-        name: "officeNumber",
-        message: "What is your Manager's office number?"
-    }
-])
-employees.push(new Manager(answers.name, answers.id, answers.email, answers.officeNumber))
+async function addManager() {
+    let answers = await inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is your Manager's name?"
+        },
+        {
+            type: 'input',
+            name: "id",
+            message: "What is your Manager's id?"
+        },
+        {
+            type: 'input',
+            name: "email",
+            message: "What is your Manager's email?"
+        },
+        {
+            type: 'input',
+            name: "officeNumber",
+            message: "What is your Manager's office number?"
+        }
+    ])
+    employees.push(new Manager(answers.name, answers.id, answers.email, answers.officeNumber))
 }
 
 // Function to add Engineer
-async function addEngineer(){
+async function addEngineer() {
     let answers = await inquirer.prompt([
         {
             type: "input",
@@ -67,43 +62,49 @@ async function addEngineer(){
         }
     ])
     employees.push(new Engineer(answers.name, answers.id, answers.email, answers.gitHub))
-        addTeamMember();
-    };
+    addTeamMember();
+};
 
 // Function to add Intern
-    async function addIntern(){
-        let answers = await inquirer.prompt([
-            {
-                type: "input",
-                name: "name",
-                message: "What is your Interns's name?"
-            },
-            {
-                type: 'input',
-                name: "id",
-                message: "What is your Intern's id?"
-            },
-            {
-                type: 'input',
-                name: "email",
-                message: "What is your Intern's email?"
-            },
-            {
-                type: 'input',
-                name: "school",
-                message: "What is your Intern's school?"
-            }
-        ])
-        employees.push(new Intern(answers.name, answers.id, answers.email, answers.school))
-            addTeamMember();
-        };
+async function addIntern() {
+    let answers = await inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is your Interns's name?"
+        },
+        {
+            type: 'input',
+            name: "id",
+            message: "What is your Intern's id?"
+        },
+        {
+            type: 'input',
+            name: "email",
+            message: "What is your Intern's email?"
+        },
+        {
+            type: 'input',
+            name: "school",
+            message: "What is your Intern's school?"
+        }
+    ])
+    employees.push(new Intern(answers.name, answers.id, answers.email, answers.school))
+    addTeamMember();
+};
+function generateHTML() {
+    console.log("OK! Enjoy your new roster.")
+    const teamPage = render(employees);
+    fs.writeFileSync('../Output/team.html', teamPage)
+}
 
- async function addTeamMember(){
+
+async function addTeamMember() {
     let answers = await inquirer.prompt([
         {
             type: 'list',
             name: 'toAdd',
-            message:'What type of team member do you want to add?',
+            message: 'What type of team member do you want to add?',
             choices: [
                 'Engineer',
                 'Intern',
@@ -111,26 +112,13 @@ async function addEngineer(){
             ]
         }
     ])
-        console.log(answers.toAdd);
-        if(answers.toAdd === 'Engineer'){
-             await addEngineer();
-        }
-        if(answers.toAdd === 'Intern'){
-            await addIntern();
-        }
-        if(answers.toAdd === 'I do not want to add any more team members'){
-            await console.log("OK! Enjoy your new roster.")
-            console.log(employees);
-            const teamPage = render(employees);
-            fs.writeFile('../Output/team.html', teamPage, err => {
-                if (err){
-                    throw err
-                }
-            });
-        } else{
-            console.log('error')
-        }
-    }
+    let userChoice = answers.toAdd
+    console.log(userChoice);
+    if (userChoice === 'Engineer') await addEngineer();
+    if (userChoice === 'Intern') await addIntern();
+    if (userChoice === 'I do not want to add any more team members') await generateHTML();
+    else console.log('error');
+}
 
 
 
